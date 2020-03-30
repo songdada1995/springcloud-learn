5391363dfbb7d62c9923acf04987a3e611eaa0c9
songdada1995 <1152745700@qq.com>
2020-03-30 23:13:10 +0800

    hystrix 提交

--- consumer/src/main/java/com/example/consumer/controller/ConsumeController.java
+++ consumer/src/main/java/com/example/consumer/controller/ConsumeController.java
@@ -1,19 +1,21 @@
 package com.example.consumer.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
+@RequestMapping("/consumer")
 @RestController
 public class ConsumeController {
 
     @Resource
     private RestTemplate restTemplate;
 
-    @RequestMapping("consumer/hello")
-    public String get() {
+    @RequestMapping("/hello")
+    public String get(@RequestParam String param) {
         return restTemplate.getForEntity("http://PROVIDER/provider/hello",String.class).getBody();
     }
 }
